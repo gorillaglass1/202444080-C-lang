498cb5f449d32076f71e98df2cafc7ab7da8948d
gorillaglass1 <155641361+gorillaglass1@users.noreply.github.com>
2024-03-19 12:27:04 +0900

    Add files via upload

---
+++ source/1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h> //system 함수를 포함하고 있음
+
+int main(void) {
+    system("title 정수형 데이터의 자릿수 지정 형식");
+
+    printf("|%d|", 358);  //1
+    printf(": 정수형 기본 자릿수\n");
+
+    printf("|%5d|", 358);
+    printf(": 전체 자릿수 5개\n");
+
+    printf("|%05d|", 358);
+    printf(": 전체 자릿수 5개이고, 빈칸은 0으로 채움\n");
+    return 0;
+}
